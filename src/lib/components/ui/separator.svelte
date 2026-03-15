06946e0c04e57cbef3249292eea63d16391eea69
songloimr <songloimr@gmail.com>
2026-03-15 19:32:41 +0700

    chore(ui): remove unused UI components
    Remove combobox, separator, slider, and tabs components that are
    no longer referenced anywhere in the application.

--- src/lib/components/ui/separator.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  interface Props {
-    class?: string;
-  }
-
-  let { class: className = '' }: Props = $props();
-</script>
-
-<div class={`h-px w-full bg-border ${className}`}></div>
